dd80e4535076c04f1083bab0c210cd22719a5b1e
in-vguillem <vguillem@sklum.com>
2022-07-06 13:58:43 +0200

    ?

--- lib/config/shared_preferences.dart
+++ lib/config/shared_preferences.dart
@@ -6,6 +6,8 @@ class SharedPrefs {
   static Future<SharedPreferences> init() async => _instance = await SharedPreferences.getInstance();
 
   static bool? getBool(String key) => _instance.getBool(key);
+  static String? getString(String key) => _instance.getString(key);
 
   static Future<bool> setBool(String key, bool value) => _instance.setBool(key, value);
+  static Future<bool> setString(String key, String value) => _instance.setString(key, value);
 }
